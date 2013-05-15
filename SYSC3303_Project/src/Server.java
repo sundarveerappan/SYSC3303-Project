4f707c8a3a77580a67f4e5faba9c89ea18cac811
Ethan Aubuchon <ethan.a.aubuchon@gmail.com>
2013-05-15 17:36:48 -0400

    Added parseRequest Method to ServerThread class

--- SYSC3303_Project/src/Server.java
+++ SYSC3303_Project/src/Server.java
@@ -8,8 +8,7 @@ public class Server {
 	public static final int WELL_KNOWN_PORT = 69;
 	public static final int BUFFER_SIZE = 512+4;
 	//public static final byte DEFAULT = 0;//Default Server will be type 0
-	public static final byte READ = 1;
-	public static final byte WRITE = 2;
+
 	
 	private DatagramSocket wellKnown;
 	private DatagramPacket incomingPacket;
@@ -27,16 +26,14 @@ public class Server {
 	
 	public void recieveTFTP() {
 		byte data[] = new byte[BUFFER_SIZE];
-		DatagramPacket incomingPacket = new DatagramPacket(data, data.length);
+		incomingPacket = new DatagramPacket(data, data.length);
 		
 		try {
 			wellKnown.receive(incomingPacket);
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.exit(1);
-		}
-		//return incomingPacket;
-		
+		}		
 		//Create ServerThread()
 	}
 } 
--- SYSC3303_Project/src/ServerThread.java
+++ SYSC3303_Project/src/ServerThread.java
@@ -2,15 +2,49 @@ import java.net.*;
 
 
 public class ServerThread extends Thread{
+	public static enum Request {ERROR, READ, WRITE};
+	
 	private DatagramPacket request;
 	private DatagramSocket socket;
-	private byte requestType;
+	private InetAddress ip;
+	private int port;
+	private String file;
+	private String mode;
+	private Request requestType;
 	
 	public ServerThread(DatagramPacket request) {
 		this.request = request;
 	}
 	
-	public void handleRequest() {
+	public void parseRequest() {
+		int length  = this.request.getLength();
+		byte data[] = this.request.getData();
+		this.ip = this.request.getAddress();
+		this.port = this.request.getPort();
 		
+		if (data[0]!=0) requestType = Request.ERROR;
+		else if (data[1]==1) requestType = Request.READ;
+		else if (data[1]==2) requestType = Request.WRITE;
+		else requestType = Request.ERROR;
+		
+		if (requestType!=Request.ERROR) {
+			//find filename
+			int fileCount;
+			for(fileCount = 2; fileCount < length; fileCount++) {
+				if (data[fileCount] == 0) break;
+			}
+			if (fileCount==length) requestType=Request.ERROR;
+			else file = new String(data,2,fileCount-2);
+			
+			//find mode
+			int modeCount;
+			for(modeCount = fileCount+1; modeCount < length; modeCount++) {
+				if (data[modeCount] == 0) break;
+			}
+			if (fileCount==length) requestType=Request.ERROR;
+			else mode = new String(data,fileCount,modeCount-fileCount-1);
+			
+			if(modeCount!=length-1) requestType=Request.ERROR;
+		}
 	}
 }
